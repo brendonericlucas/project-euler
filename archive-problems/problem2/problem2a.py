af9aa686affab1118d355cfecf9e11de1c5f42bd
Brendon Eric Lucas <brendon.eric.lucas@gmail.com>
2020-06-03 20:40:25 -0400

    Solution to problem 2

---
+++ archive-problems/problem2/problem2a.py
@@ -0,0 +1,16 @@
+def sum_even_fibs(upper):
+    """
+      computes the sum of the 
+      even fibonacci numbers
+      using dynamic programming
+    """
+    f1, f2, f, sum = 1, 2, 0, 2
+    while f <= 4000000:
+        f = f1 + f2
+        f1 = f2
+        f2 = f
+        sum += (f % 2 == 0)*f
+    return sum
+
+print(sum_even_fibs(4000000))
+    
